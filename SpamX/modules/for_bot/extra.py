0b6be12288a6d8fc7c4e913480d2de87babc8061
itskapilrao <itskapilrao@gmail.com>
2025-07-13 11:05:57 +0530

    extra.py

--- SpamX/modules/for_bot/extra.py
+++ SpamX/modules/for_bot/extra.py
@@ -24,7 +24,7 @@ async def uspam(SpamX: Client, e: Message):
        await e.reply("Gime Spam message bruh!")
        return
     if re.search(res_devs.lower(), msg.lower()):
-       await e.reply("**Sorry !!** I can't Spam On @SHIVANSH39's owner")
+       await e.reply("**Sorry !!** I can't Spam On @ItsKapilYadav's owner")
        return
 
     try:
